f3eff7c73f0fb61562879577b93d832efbd9b0e5
Rof1yev <rofiyevdilshod@gmail.com>
2023-11-13 14:13:03 +0500

    header change

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/layout";
+import { Sidebar } from "@/components";
 import Head from "next/head";
 
 export default function Search(): JSX.Element {
@@ -7,11 +7,22 @@ export default function Search(): JSX.Element {
       <Head>
         <title>Instagram | Search</title>
       </Head>
-      <Layout>
-        <main>
+      <div
+        style={{
+          display: "flex",
+          maxWidth: "1500px",
+          margin: "0 auto",
+          overflow: "hidden",
+          minHeight: "100vh",
+        }}
+      >
+        <section>
+          <Sidebar />
+        </section>
+        <main style={{ padding: "20px", width: "100%" }}>
           <h1>Search</h1>
         </main>
-      </Layout>
+      </div>
     </>
   );
 }
